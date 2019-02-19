09098d922b86c6cadcd0b0c93dd73dc00cb9eabb
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2019-02-19 14:08:05 +0100

    dodano wsparcie dla amd

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const client = new Discord.Client();
 client.on("message", (message) => {
 	if(message.content.toLowerCase().includes("intel")) {
 		message.react("id reakcji");
+	} else if (message.content.toLowerCase().includes("amd")) {
+		message.react("id reakcji");
 	}
 });
  
