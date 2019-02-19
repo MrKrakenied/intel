1fd71c037e7fa0b1ecdd25e47d60974606dfd06a
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2019-02-19 14:02:51 +0100

    wsparcie dla wielkich liter

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", (message) => {
-	if (message.content.includes("intel")) {
+	if(message.content.toLowerCase().includes("intel")) {
 		message.react("id reakcji");
 	}
 });
