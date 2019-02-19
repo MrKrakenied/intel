d6ce3031aa1adf0b65cd1a0399020885fc6f6b70
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2019-02-19 13:50:52 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", (message) => {
+	if (message.content.includes("intel")) {
+		message.react("id reakcji")
+	}
+});
+ 
+client.login("token bota");
