5d1b93497db37509dd22db1c31c0cf34d785601f
MrKrakenied <46192742+MrKrakenied@users.noreply.github.com>
2019-02-19 22:17:59 +0100

    usuwam zbędną spację
    dla uwydajnienia kodu kek

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on("message", (message) => {
 	if(message.content.toLowerCase().includes("intel")) {
 		message.react("id reakcji");
-	} else if (message.content.toLowerCase().includes("amd")) {
+	} else if(message.content.toLowerCase().includes("amd")) {
 		message.react("id reakcji");
 	}
 });
